caf6e1e3bb0bb75b89c8fb2d7035a3fe6cba0b48
Jason Haas <Jasonbhaas@gmail.com>
2019-05-09 17:41:28 -0700

    added images for products

--- deal-tracker/src/App.js
+++ deal-tracker/src/App.js
@@ -6,13 +6,13 @@ var products = [
     name: "Achilles Low Top",
     brand: "Commong Projects",
     price: 400,
-    image: "images/shoes.jpeg"
+    image: "/images/shoes.jpeg"
   },
   {
     name: "Kensington Slim Fit Floral Print Cotton Sport Shirt",
     brand: "Good Man Brand",
     price: 168,
-    image: "images/shoes.jpeg"
+    image: '/images/shirt.jpeg'
   }
 ]
 
@@ -39,7 +39,7 @@ class ProductCard extends React.Component {
     return (
       <div className="row">
         <div className="col-md-5 item-pic">
-          <img src={this.props.product.image} />
+          <img src={this.props.product.image} width='250px' />
         </div>
         <div className="col-md-7 item-desc">
           <h2>{this.props.product.name}</h2>
