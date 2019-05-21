7dabda7e18081a985f55cc92ba2f1390dbfd512f
Jason Haas <Jasonbhaas@gmail.com>
2019-05-21 11:42:29 -0700

    fixed run scripts

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,64 +1,18 @@
-import React from 'react';
-import './App.css';
-import { read } from 'fs';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AppNavbar from "./components/AppNavbar";
 
-//test comment
-
-var products = [
-  {
-    name: "Achilles Low Top",
-    brand: "Commong Projects",
-    price: 400,
-    image: "/images/shoes.jpeg"
-  },
-  {
-    name: "Kensington Slim Fit Floral Print Cotton Sport Shirt",
-    brand: "Good Man Brand",
-    price: 168,
-    image: '/images/shirt.jpeg'
-  }
-]
+import { Container } from "reactstrap";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container mt-4">
-        <Feed></Feed>
-      </div>
-    )
-  }
-}
-
-
-class Feed extends React.Component {
-  render() {
-    const Cards = products.map((x) => {
-      return (
-        <li>
-          <ProductCard product={x} />
-        </li>
-      )
-    });
-    return (
-      <ul>
-        {Cards}
-      </ul>
-    );
-  }
-}
-
-class ProductCard extends React.Component {
-  render() {
-    return (
-      <div className="row card mt-3">
-        <div className="col-md-5 item-pic">
-          <img src={this.props.product.image} width='250px' />
-        </div>
-        <div className="col-md-7 item-desc">
-          <h2>{this.props.product.name}</h2>
-          <h3>{this.props.product.brand}</h3>
-          <ul><li>${this.props.product.price}</li></ul>
-        </div>
+      <div className='App'>
+        <AppNavbar />
+        <Container>
+          <h1>Hello World</h1>
+        </Container>
       </div>
     );
   }
---
+++ Client/src/components/Feed.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+var products = [
+  {
+    name: "Achilles Low Top",
+    brand: "Commong Projects",
+    price: 400,
+    image: "/images/shoes.jpeg"
+  },
+  {
+    name: "Kensington Slim Fit Floral Print Cotton Sport Shirt",
+    brand: "Good Man Brand",
+    price: 168,
+    image: "/images/shirt.jpeg"
+  }
+];
+
+class Feed extends React.Component {
+  render() {
+    const Cards = products.map(x => {
+      return (
+        <li>
+          <ProductCard product={x} />
+        </li>
+      );
+    });
+    return <ul>{Cards}</ul>;
+  }
+}
+
+class ProductCard extends React.Component {
+  render() {
+    return (
+      <div className='row card mt-3'>
+        <div className='col-md-5 item-pic'>
+          <img src={this.props.product.image} width='250px' />
+        </div>
+        <div className='col-md-7 item-desc'>
+          <h2>{this.props.product.name}</h2>
+          <h3>{this.props.product.brand}</h3>
+          <ul>
+            <li>${this.props.product.price}</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
