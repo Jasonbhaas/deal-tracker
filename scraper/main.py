a0636ef6b2eb12608c2450c8d43a34fba452b796
Jason Haas <Jasonbhaas@gmail.com>
2019-06-03 15:23:19 -0700

    fixed date issue

--- scraper/main.py
+++ scraper/main.py
@@ -3,25 +3,25 @@ from pymongo import InsertOne, DeleteMany, ReplaceOne, UpdateOne
 from bson.objectid import ObjectId
 from scraper import get_price
 import pprint
+import datetime
 
 url = "mongodb+srv://jason:jason123@cluster0-5lvli.mongodb.net/dev?retryWrites=true&w=majority"
 
 client = MongoClient(url)
-# db = client.dealTrackerDev
 items = client.dev.items
-batch = []
-
-for item in items.find():
-    pprint.pprint(item['name'])
 
 # for item in items.find():
-#     try:
-#         price = get_price(item['url'], item['keyword'])
-#         print(price)
-#         items.update_one({'_id': item['_id']},
-#                          {'$push': {'priceHistory': {'price': float(price)}}})
-#     except:
-#         print("error!")
+#     pprint.pprint(item['name'])
+
+for item in items.find():
+    try:
+        price = get_price(item['url'], item['keyword'])
+        print(price)
+        items.update_one({'_id': item['_id']},
+                         {'$push': {'priceHistory': {'price': float(price),
+                                                     'date': datetime.datetime.now()}}})
+    except:
+        print("error!")
 
 # batch.append(UpdateOne({'_id': item['_id']}, {
     #     '$push': {'priceHistory': price}}))
--- scraper/seed.py
+++ scraper/seed.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import datetime
 
 url = "mongodb+srv://jason:jason123@cluster0-5lvli.mongodb.net/dev?retryWrites=true&w=majority"
 
@@ -9,15 +10,17 @@ items = [{
     "name": "Achilles Low",
     "make": "Common Projects",
     "image": "images/shoes.jpeg",
-    "url": "https://shop.nordstrom.com/s/common-projects-original-achilles-sneaker-men/4976450",
-    "keyword": "_2cY3bT"
+    "url": "https://www.mrporter.com/en-us/mens/product/common_projects/original-achilles-leather-sneakers/903868",
+    "keyword": "product-details__price--value",
+    "date": datetime.datetime.now()
 },
     {
     "name": "Kensington Slim Fit Floral Print Cotton Sport Shirt",
     "make": "Good Man Brand",
     "image": "images/shirt.jpeg",
     "url": "https://shop.nordstrom.com/s/good-man-brand-kensington-slim-fit-floral-print-cotton-sport-shirt/5139306",
-    "keyword": "_2cY3bT"
+    "keyword": "_2cY3bT",
+    "date": datetime.datetime.now()
 },
     {
     "name": "L'Homme Slim Fit Jeans",
@@ -25,13 +28,15 @@ items = [{
     "image": "images/jeans.jpeg",
     "url": "https://shop.nordstrom.com/s/frame-lhomme-slim-fit-jeans-bradbury/4770233",
     "keyword": "_2cY3bT",
+    "date": datetime.datetime.now()
 },
     {
     "name": "Peruvian Pima Cotton Robe",
     "make": "Daniel Buchler",
     "image": "images/robe.jpeg",
     "url": "https://shop.nordstrom.com/s/daniel-buchler-peruvian-pima-cotton-robe/3675851",
-    "keyword": "_2cY3bT"
+    "keyword": "_2cY3bT",
+    "date": datetime.datetime.now()
 }]
 
 if __name__ == "__main__":
