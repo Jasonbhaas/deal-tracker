923c4d648d06d6ef2ef97218ab214f61b477bc4d
Jason Haas <Jasonbhaas@gmail.com>
2019-07-11 16:04:04 -0700

    changed ports to 3000 and 5000

--- server.js
+++ server.js
@@ -20,15 +20,18 @@ app.use(express.json());
 app.use("/api/items", require("./routes/api/items.js"));
 app.use("/api/users", require("./routes/api/users.js"));
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 // Serve static assets if in production
 
-// Set static folder
-app.use(express.static("client/build"));
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
