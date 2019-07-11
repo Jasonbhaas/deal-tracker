583853a268fd80c8049748de87346503a3b7d11c
Jason Haas <Jasonbhaas@gmail.com>
2019-07-11 10:03:10 -0700

    preparing for deploy

--- server.js
+++ server.js
@@ -20,17 +20,15 @@ app.use(express.json());
 app.use("/api/items", require("./routes/api/items.js"));
 app.use("/api/users", require("./routes/api/users.js"));
 
+const PORT = process.env.PORT || 8000;
+
 // Serve static assets if in production
 
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+// Set static folder
+app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
-const PORT = process.env.PORT || 5000;
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
